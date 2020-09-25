62771be5639cee3caf96ef0e7314bc718b8209b8
mboisvenue <MTBoisvenue@yahoo.com>
2020-09-25 10:36:18 -0400

    Wrote functionality for signup and login buttons

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 const forms = document.querySelectorAll('.form');
-const signUpContainer = document.getElementById('signup-container');
+const modalOpen = document.querySelectorAll('.modal-open');
 
 var signupBtn = forms[0];
 var loginBtn = forms[1];
 
+const openSignup = modalOpen[0];
+const openLogin = modalOpen[1];
 
+console.log(openSignup);
 signupBtn.addEventListener('click', e=> {
     e.preventDefault();
-    signUpContainer.style.visibility = "visible";
+    openSignup.style.display = "block";
+
 });
 
 loginBtn.addEventListener('click', e=> {
     e.preventDefault();
+    openLogin.style.display = "block";
 });
+
+window.addEventListener('click', e=>{
+    if(e.target === openSignup){
+        openSignup.style.display = "none";
+    } else if(e.target === openLogin) {
+        openLogin.style.display = "none";
+    } else {
+        return false;
+    }
+});
+
 
